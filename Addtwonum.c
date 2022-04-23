8f81854f7fa2f3fe2629e2aca1bbafc143456c6a
amithhd <103755649+amithhd@users.noreply.github.com>
2022-04-23 11:07:05 +0530

    Update Addtwonum.c

--- Addtwonum.c
+++ Addtwonum.c
@@ -1,1 +1,17 @@
+#include<stdio.h>
+
+void main
+{
+    int a,b;
+    int sum=0;
+    printf("Enter the value for A== >");
+    scanf("%d",&a);
+    printf("Enter the value for B==> ");
+    scanf("%d",&b);
+    
+    sum=a+b;
+    printf("The sum of two numbers is==> %d",sum;
+    
+    return 0;
+}
 
